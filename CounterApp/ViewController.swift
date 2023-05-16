57405ad0c32d0405e80670343181133a3f478384
Bogdan Fartdinov <bogdan6246@yandex.ru>
2023-05-16 20:33:02 +0300

    В  ViewController добавленные методы увеличения, уменьшения, обнуления счетчика и вывод  истории изменений.

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterChangeLog: UITextView!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    private var counter: Int = 0
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Обновление счетчика нажатий
+        updateCounter()
+        counterChangeLog.text = "История измененй:\n"
+        
+        
     }
-
-
+    //MARK: - настройки счетчика и даты
+    private func updateCounter() {
+        countLabel.text = "Значение счётчика: \(counter)"
+    }
+    
+    private func setupDate() -> String {
+        let dateFormatter = DateFormatter()
+        let date = Date.now
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+        return (dateFormatter.string(from: date))
+    }
+    
+    
+    //MARK: - Обработчики событий
+    @IBAction func decreaseButtonTapped(_ sender: UIButton) {
+        let date = setupDate()
+        if counter > 0 {
+            counter -= 1
+            updateCounter()
+            counterChangeLog.text.append("\(date) значение изменено на -1\n")
+        } else {
+            counterChangeLog.text.append("\(date) попытка уменьшить значение счётчика ниже 0\n")
+        }
+    }
+    
+    @IBAction func encreaseButtonTapped(_ sender: UIButton) {
+        let date = setupDate()
+        counter += 1
+        updateCounter()
+        counterChangeLog.text.append("\(date) значение изменено на +1\n")
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+        let date = setupDate()
+        counter = 0
+        updateCounter()
+        counterChangeLog.text.append("\(date) значение сброшено\n")
+    }
 }
 
